5808a66b7dc8610b4a353f36dc7f61d26c2e92db
GregClement <gregc@elevatehomescriptions.com>
2024-01-06 16:01:24 -0700

    .

---
+++ python/start.py
@@ -0,0 +1,36 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        with open('template.html', 'r') as file:
+            # Read the contents of the file
+            contents = file.read()
+
+        # Print the contents of the file
+        print(contents)
+        self.wfile.write(bytes(contents, "utf-8"))
+"""         self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<h1>This is an example web server.</h1>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8")) """
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
