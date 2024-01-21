055549c76d3ebbd2fa426503f756c3194435d140
Greg Clement <gregb_clement@hotmail.com>
2024-01-20 19:07:19 -0700

    .

--- python/start.py
+++ python/start.py
@@ -1,8 +1,14 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+import RPi.GPIO as GPIO
+import json
 
 hostName = "localhost"
 serverPort = 8080
+
+BEAM_PIN = 17
+scores = []
+
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -10,27 +16,42 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
-        with open('template.html', 'r') as file:
-            # Read the contents of the file
-            contents = file.read()
+        print("Address:")
+        print(self.path)
+        if self.path == "/exchange-updates":
+            self.wfile.write(bytes(json.dumps(scores), "utf-8"))
+            scores.clear()
+        else:
+            with open('template.html', 'r') as file:
+                contents = file.read()
 
-        # Print the contents of the file
-        print(contents)
-        self.wfile.write(bytes(contents, "utf-8"))
-"""         self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<h1>This is an example web server.</h1>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8")) """
+            self.wfile.write(bytes(contents, "utf-8"))
+
+
+
+def break_beam_callback(channel):
+    if GPIO.input(BEAM_PIN):
+        print("beam unbroken")
+    else:
+        print("beam broken")
+        msg = {
+            "sender": "Server",
+            "messageType": "PLAYER_1_SCORED"
+        }
+        scores.append(msg)
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(BEAM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(BEAM_PIN, GPIO.BOTH, callback=break_beam_callback)
         webServer.serve_forever()
     except KeyboardInterrupt:
         pass
 
     webServer.server_close()
+    GPIO.cleanup()
     print("Server stopped.")
