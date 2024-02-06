d40f0f4a6c03835de696435819a07d5391f91cbe
Greg Clement <g.clement@upkeeplabs.com>
2024-02-06 09:48:04 -0700

    Font changes

--- src/app/components/game-setup/game-setup.component.ts
+++ src/app/components/game-setup/game-setup.component.ts
@@ -25,9 +25,9 @@ export class GameSetupComponent implements OnChanges {
   private joystick2 = new JoystickState(1);
   gameTypeSelected = false;
   gameTypes: GameType[] = [
-    { type: 'Virtual', lottieUrl: 'https://lottie.host/2624709a-b10c-43e1-af2a-623d1434c3b3/HhWUwa1d3c.json', description: 'Play pong using the joystick' },
+    { type: 'Virtual', lottieUrl: 'https://lottie.host/2624709a-b10c-43e1-af2a-623d1434c3b3/HhWUwa1d3c.json', description: 'Play pong using the joysticks' },
     { type: 'Physical', lottieUrl: 'https://lottie.host/063f9150-34fc-4bdc-92df-16318a0f3a79/xG6AAyhFbC.json', description: 'Play air hockey on the real table' },
-    { type: 'Both', lottieUrl: 'https://lottie.host/a7044b1d-7b7c-4dbe-8c08-f8579798acd4/pnWDVXLTWJ.json', description: 'Both pong and the real table simultaneously' }
+    { type: 'Both', lottieUrl: 'https://lottie.host/a7044b1d-7b7c-4dbe-8c08-f8579798acd4/pnWDVXLTWJ.json', description: 'Play both pong and the real table simultaneously' }
   ];
 
   constructor() {
